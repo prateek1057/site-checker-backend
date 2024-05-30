9ffa7e8eee80c80b01541cf9b341d24481c75f62
prateek1057 <sprateek457@gmail.com>
2024-05-30 11:53:06 +0530

    30 may commit

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup, SoupStrainer
 from flask_cors import CORS
 import urllib.request
 from time import time
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
@@ -65,6 +67,59 @@ def get_misspelledwords_from_website(url):
         return misspelled_words
     else:
         return "No Misspelled word Found"
+    
+def get_image_src_links(url):
+    response = requests.get(url)
+    src_links = []
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    img_tags = soup.find_all('img')
+
+    for img in img_tags:
+        if img.has_attr('src'):
+            if(img['src']=='/MS logo.png'):
+                src_links.append('https://smt.microsoft.com/MS%20logo.png')
+            else:
+                src_links.append(img['src'])
+
+    return src_links
+
+def is_blurry(image_url, threshold=100):
+    # Download the image using requests
+    response = requests.get(image_url)
+    
+    if response.status_code == 200:
+        # Convert the downloaded content to a NumPy array
+        image_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+        
+        # Decode the image array using OpenCV
+        image = cv2.imdecode(image_array, cv2.IMREAD_GRAYSCALE)
+        
+        # Apply the Laplacian operator
+        laplacian_var = cv2.Laplacian(image, cv2.CV_64F).var()
+        # print(laplacian_var)
+        # If the variance is less than the threshold, the image is considered blurry
+        return laplacian_var < threshold
+    else:
+        print("Failed to download the image from the URL")
+        return False
+
+@app.route('/check_blurry_images', methods=['POST'])   
+def check_web_images():
+    data = request.get_json()
+    url = data['url']
+    blurry_images=[]
+    image_urls=get_image_src_links(url)
+    for image_url in image_urls:
+        if(is_blurry(image_url,100)):
+            blurry_images.append(image_url)
+        else:
+            print("Image is not blurry")
+
+    if len(blurry_images)==0:
+        return jsonify({"Message":"No Blurry Image Found"})
+    else:
+        return jsonify({"Blurry Images Links:" :blurry_images})
 
 @app.route('/check_website_links', methods=['POST'])
 def check_website_links():
@@ -91,7 +146,8 @@ def check_load_time():
     end_time = time()
     stream.close()
     load_time = end_time - start_time
-    return f"Page load time of Website is: {load_time:.3f}"
+    return output
+    # return f"Page load time of Website is: {load_time:.3f}"
 
 if __name__ == '__main__':
     app.run(debug=True)
