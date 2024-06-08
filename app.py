32cc906bee5e5f88574383aaf8ef7bc5280fa734
prateek1057 <sprateek457@gmail.com>
2024-06-09 01:04:06 +0530

    Reducing size commit

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from bs4 import BeautifulSoup, SoupStrainer
 from flask_cors import CORS
 import urllib.request
 from time import time
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
 from collections import defaultdict
 from spellchecker import SpellChecker
-from urllib.parse import urlparse, urlunparse
+# from urllib.parse import urlparse, urlunparse
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
@@ -82,57 +82,57 @@ def get_misspelled_words(html_code):
     
     return all_words_count, correct_words_count, misspelled_words, misspelled_word_with_correct_word
 
-def get_image_src_links(html_code):
-    src_links = []
+# def get_image_src_links(html_code):
+#     src_links = []
 
-    soup = BeautifulSoup(html_code, 'html.parser')
-    img_tags = soup.find_all('img')
+#     soup = BeautifulSoup(html_code, 'html.parser')
+#     img_tags = soup.find_all('img')
 
-    for img in img_tags:
-        if img.has_attr('src'):
-            if(img['src']=='/MS logo.png'):
-                src_links.append('https://smt.microsoft.com/MS%20logo.png')
-            else:
-                src_links.append(img['src'])
+#     for img in img_tags:
+#         if img.has_attr('src'):
+#             if(img['src']=='/MS logo.png'):
+#                 src_links.append('https://smt.microsoft.com/MS%20logo.png')
+#             else:
+#                 src_links.append(img['src'])
 
-    return src_links
+#     return src_links
 
-def is_blurry(image_url, threshold=100):
-    def calculate_laplacian_variance(image):
-        # Apply the Laplacian operator and return the variance
-        return cv2.Laplacian(image, cv2.CV_64F).var()
+# def is_blurry(image_url, threshold=100):
+#     def calculate_laplacian_variance(image):
+#         # Apply the Laplacian operator and return the variance
+#         return cv2.Laplacian(image, cv2.CV_64F).var()
     
-    def resize_image(image, size=(500, 500)):
-        # Resize image while maintaining the aspect ratio
-        h, w = image.shape[:2]
-        if h > w:
-            new_h, new_w = size[0], int(size[0] * w / h)
-        else:
-            new_h, new_w = int(size[1] * h / w), size[1]
-        return cv2.resize(image, (new_w, new_h))
+#     def resize_image(image, size=(500, 500)):
+#         # Resize image while maintaining the aspect ratio
+#         h, w = image.shape[:2]
+#         if h > w:
+#             new_h, new_w = size[0], int(size[0] * w / h)
+#         else:
+#             new_h, new_w = int(size[1] * h / w), size[1]
+#         return cv2.resize(image, (new_w, new_h))
     
-    # Download the image using requests
-    response = requests.get(image_url)
+#     # Download the image using requests
+#     response = requests.get(image_url)
     
-    if response.status_code == 200:
-        # Convert the downloaded content to a NumPy array
-        image_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+#     if response.status_code == 200:
+#         # Convert the downloaded content to a NumPy array
+#         image_array = np.asarray(bytearray(response.content), dtype=np.uint8)
         
-        # Decode the image array using OpenCV
-        image = cv2.imdecode(image_array, cv2.IMREAD_GRAYSCALE)
+#         # Decode the image array using OpenCV
+#         image = cv2.imdecode(image_array, cv2.IMREAD_GRAYSCALE)
         
-        # Resize the image to a standard size for consistency
-        image = resize_image(image)
+#         # Resize the image to a standard size for consistency
+#         image = resize_image(image)
         
-        # Calculate the Laplacian variance
-        laplacian_var = calculate_laplacian_variance(image)
+#         # Calculate the Laplacian variance
+#         laplacian_var = calculate_laplacian_variance(image)
         
-        # Determine if the image is blurry
-        is_blurry = laplacian_var < threshold
-        return is_blurry
-    else:
-        print("Failed to download the image from the URL")
-        return False
+#         # Determine if the image is blurry
+#         is_blurry = laplacian_var < threshold
+#         return is_blurry
+#     else:
+#         print("Failed to download the image from the URL")
+#         return False
     
 def validate_html_w3c(html_code):
     headers = {
@@ -157,33 +157,33 @@ def validate_html_w3c(html_code):
     
     return errors,warnings
 
-def get_partial_url(full_url):
-    # Parse the full URL
-    parsed_url = urlparse(full_url)
-    # Extract the scheme and netloc (base URL)
-    base_url = urlunparse((parsed_url.scheme, parsed_url.netloc, '', '', '', ''))
-    return base_url
+# def get_partial_url(full_url):
+#     # Parse the full URL
+#     parsed_url = urlparse(full_url)
+#     # Extract the scheme and netloc (base URL)
+#     base_url = urlunparse((parsed_url.scheme, parsed_url.netloc, '', '', '', ''))
+#     return base_url
 
-@app.route('/check_blurry_images', methods=['POST'])   
-def check_web_images():
-    data = request.get_json()
-    html_code = data['code']
-    url=data['url']
-    blurry_images=[]
-    image_urls=get_image_src_links(html_code)
-    for image_url in image_urls:
-        if image_url.startswith("https://"):
-            if(is_blurry(image_url,100)):
-                blurry_images.append(image_url)
-        else:
-            partial_url=get_partial_url(url)
-            full_image_url=partial_url+image_url
-            if(is_blurry(full_image_url,100)):
-                blurry_images.append(full_image_url)
-    if len(blurry_images)==0:
-        return jsonify({"Message":"No Blurry Image Found"})
-    else:
-        return jsonify({"Blurry Images Links:" :blurry_images})
+# @app.route('/check_blurry_images', methods=['POST'])   
+# def check_web_images():
+#     data = request.get_json()
+#     html_code = data['code']
+#     url=data['url']
+#     blurry_images=[]
+#     image_urls=get_image_src_links(html_code)
+#     for image_url in image_urls:
+#         if image_url.startswith("https://"):
+#             if(is_blurry(image_url,100)):
+#                 blurry_images.append(image_url)
+#         else:
+#             partial_url=get_partial_url(url)
+#             full_image_url=partial_url+image_url
+#             if(is_blurry(full_image_url,100)):
+#                 blurry_images.append(full_image_url)
+#     if len(blurry_images)==0:
+#         return jsonify({"Message":"No Blurry Image Found"})
+#     else:
+#         return jsonify({"Blurry Images Links:" :blurry_images})
 
 @app.route('/check_website_links', methods=['POST'])
 def check_website_links():
